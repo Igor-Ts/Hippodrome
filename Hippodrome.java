30f1c1e1d9e89bef39fb2b24a1a76d0787d07ff6
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-06-27 18:35:35 +0300

    End task3. Added ArrayList Horse and getter Start task 4

--- Hippodrome.java
+++ Hippodrome.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 public class Hippodrome {
+    ArrayList<Horse> horses = new ArrayList<>();
+
+    public ArrayList<Horse> getHorses() {
+        return horses;
+    }
+
     public static void main(String[] args) {
 
     }
