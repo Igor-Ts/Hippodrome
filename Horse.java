773487f05d9ce973e62aa88d2c2aff2b73359ded
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-06-27 18:46:53 +0300

    End task5. Added into class Horse constructor, and all getters and setters Start task 6

--- Horse.java
+++ Horse.java
@@ -1,5 +1,36 @@
 public class Horse {
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
     public String name;
     public double speed;
     public double distance;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+
 }
