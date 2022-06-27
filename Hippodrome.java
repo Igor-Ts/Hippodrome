6b543b61409cd1b40108b08ce7623e83de70ec89
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-06-27 20:34:16 +0300

    End task6. Added 3 horses Start task 7

--- Hippodrome.java
+++ Hippodrome.java
@@ -2,12 +2,20 @@ import java.util.ArrayList;
 
 public class Hippodrome {
     ArrayList<Horse> horses = new ArrayList<>();
+    public static Hippodrome game;
 
     public ArrayList<Horse> getHorses() {
         return horses;
     }
 
     public static void main(String[] args) {
-
+        Hippodrome hippodrome = new Hippodrome();
+        game = hippodrome;
+        Horse Appalon = new Horse("Appalon",3,0);
+        Horse Victory = new Horse("Victory",3,0);
+        Horse Gambit = new Horse("Gambit",3,0);
+        hippodrome.getHorses().add(Appalon);
+        hippodrome.getHorses().add(Victory);
+        hippodrome.getHorses().add(Gambit);
     }
 }
